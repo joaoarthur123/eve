83987b295b66c42d9a2a3f5bd49f0512de23edf6
joaoarthur123 <142457285+joaoarthur123@users.noreply.github.com>
2023-08-22 21:48:31 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+let variaveltexto ="texto";
+let variavelnumero = 6;
+console.log (typeof variaveltexto);
+console.log(typeof variavelnumero);
+
+//variavel vazia
+let variavelVazia;
+console.log(typeof variavelVazia);
+let variavelNula = null;
+console.log(variavelNula);
+
+let a = "b";
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
+
+//exercicio 2
+
+let nome;
+let idade;
+let numero = 6;
+nome = prompt("qual é o seu nome");
+idade = prompt("qual é sua idade");
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(numero);
+console.log(idade.toString());
+
+
+//conversao numero
+let n = "42"
+console.log(Number (n));
+console.log(typeof n);
+console.log(typeof Number (n));
