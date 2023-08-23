f367032d1dd5a6c61035c1774854567c974a736e
joaoarthur123 <142457285+joaoarthur123@users.noreply.github.com>
2023-08-22 21:46:27 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,21 @@
+/*
+*joao arthur 
+*
+*/
+
+// 1.b =10; a =10 b=5
+
+//2. a=10; b=10; c=10
+
+//3.nome melhor para p é= horas trabalhadas; nome melhor para t é=diaria ganha 
+
+
+//exercicio de escrita de codigo 
+
+let nome;
+let idade;
+console.log(typeof nome);
+console.log(typeof idade);
+// nao atribui o valor
+mome = prompt("qual é o seu nome");
+idade = prompt("qual é a sua idade");
